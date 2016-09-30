c9bd9459a518584ba45c8220d1283102fe1087ad
Cheryl Porter <cporter@ufl.edu>
2016-09-30 16:08:27 -0400

    Read real weather file from current directory
    - print output to file
    - output weather data plus SumRDi
    - include sample output file in repo

--- Main.f90
+++ Main.f90
@@ -10,26 +10,35 @@ Program Main
   real SumRDi
   real, dimension(500) :: TFi           !Vector of cultivar parameters
   character(30) CultivarID
+  character(12) Weather
+  character(1) text
 
   CultivarID = "Jamapa"
+  Weather = "CCPO1201.WTH"
 
 ! initialization
   call TFlower_init(CultivarID, TFi, SumRDi)
 
+  open(20,File=Weather,Status='OLD')
+  read(20,'(4/,a)') text    !skip first 5 lines
+  open(30,File="Anthesis.OUT",status='REPLACE')
+  write(30,'(a)') "  yrdoy  srad  tmin  tmax  SumRDi"
+
 ! rate
-  do doy = 1, 365
+  do i = 1, 365
+
     Dayi = 12.0
-    Sradi = 12.0
-    call RANDOM_NUMBER(x) !uniform dist, 0-1
-    Tmini = 20 + (x-0.5)*2.
-    Tmaxi = Tmini + 5
+    read(20,'(i5,3F6.0)') DOY, Sradi, Tmaxi, Tmini   
+    
+    !Sradi = 12.0
+    !call RANDOM_NUMBER(x) !uniform dist, 0-1
+    !Tmini = 20 + (x-0.5)*2.
+    !Tmaxi = Tmini + 5
     
     call TFlower_rate (Dayi, Sradi, Tmini, Tmaxi, TFi, SumRDi)
+    write(30,'(i7,3f6.2,f8.4)') DOY, Sradi, Tmini, Tmaxi, SumRDi
     if (SumRDi > 1.0) then
-      print *,"DOY = ", DOY, SumRDi, "    Anthesis"
       exit
-    else
-      print *,"DOY = ", DOY, SumRDi
     endif
   enddo
 
