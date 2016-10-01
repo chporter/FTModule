8428509eb158110b8b37648319b214fe5ae358b9
Cheryl Porter <cporter@ufl.edu>
2016-10-01 15:15:28 -0400

    Starting date, count of days after planting added

--- Main.f90
+++ Main.f90
@@ -4,7 +4,7 @@ Program Main
 
   USE AnthesisRate
 
-  integer doy
+  integer doy, SDat, YRDOY, YR, ADAP
   real x
   real Dayi, Sradi, Tmini, Tmaxi
   real SumRDi
@@ -18,21 +18,30 @@ Program Main
 
 ! initialization
   call TFlower_init(CultivarID, TFi, SumRDi)
-
+  
+! Set sowing/start day of year for flowering model to start
+  SDat = 41
+  ADAP = 0
+  
   open(20,File=Weather,Status='OLD')
   read(20,'(4/,a)') text    !skip first 5 lines
   open(30,File="Anthesis.OUT",status='REPLACE')
-  write(30,'(a)') "  yrdoy  srad  tmin  tmax  SumRDi"
+  write(30,'(a)') "adap yr doy  srad  tmin  tmax  SumRDi"
 
 ! rate
   do i = 1, 365
 
     Dayi = 12.0
-    read(20,'(i5,3F6.0)') DOY, Sradi, Tmaxi, Tmini   
-    call TFlower_rate (Dayi, Sradi, Tmini, Tmaxi, TFi, SumRDi)
-    write(30,'(i7,3f6.2,f8.4)') DOY, Sradi, Tmini, Tmaxi, SumRDi
-    if (SumRDi > 1.0) then
-      exit
+    read(20,'(i5,3F6.0)') YRDOY, Sradi, Tmaxi, Tmini 
+    DOY = MOD(YRDOY,1000)
+    YR = (YRDOY - DOY)/1000
+    If (DOY > SDat-1) Then
+      ADAP = ADAP + 1
+      call TFlower_rate (Dayi, Sradi, Tmini, Tmaxi, TFi, SumRDi)
+      write(30,'(i4,1X,i2,1X,I3.3,3f6.2,f8.4)') ADAP, YR, DOY, Sradi, Tmini, Tmaxi, SumRDi
+      if (SumRDi > 1.0) then
+        exit
+      endif
     endif
   enddo
 
