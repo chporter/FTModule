5437d7b903bf77df92c7ba3b95d6ec145da89cff
Cheryl Porter <cporter@ufl.edu>
2016-10-17 09:43:39 -0400

    Jim Jones changes
    - Remove read weather data, add loops for sensitivity analysis
    - Hardwire cultivar coefficients

--- Main.f90
+++ Main.f90
@@ -1,52 +1,93 @@
 !   Driver for testing Flowering Time Module
-
 !   Written, 2016-09-27 CHPorter, Jim Jones
-
 
 Program Main
 
-  USE AnthesisRate
+!  USE AnthesisRate
 
-  integer doy, SDat, YRDOY, YR, ADAP
+  integer DOY, SDat, YRDOY, YR
   real x
   real Dayi, Sradi, Tmini, Tmaxi
-  real SumRDi
-  real, dimension(500) :: TFi           !Vector of cultivar parameters
-  character(30) CultivarID
+  real SumRDi, Fdoy, ADAP
+  real, dimension(12) :: TFi           !Vector of cultivar parameters
+  character(8) CultivarID
   character(12) Weather
   character(1) text
 
-  CultivarID = "Jamapa"
-  Weather = "CCPO1201.WTH"
+  CultivarID = " Calima "
+  TFi(1)=+1.0
+  TFi(2)=+1.0
+  TFi(3)=+1.0
+  TFi(4)=+1.0
+  TFi(5)=+1.0
+  TFi(6)=+1.0
+  TFi(7)=+1.0
+  TFi(8)=+1.0
+  TFi(9)=+1.0
+  TFi(10)=+1.0
+  TFi(11)=+1.0
+  TFi(12)=+1.0  
+  
+!  Weather = "CCPO1201.WTH"
 
 ! initialization
-  call TFlower_init(CultivarID, TFi, SumRDi)
+
+  open(30,File="Anthesis.OUT",status='REPLACE')
+  write(30,'(a)') "adap yr doy  srad  tmin  tmax  SumRDi"
+  open(40,File='Sens.Temp.OUT',status='REPLACE')
+  write(40,'(a)') "   Cultivar'  DayLength  Srad     Tmin     Tmax    SumRDi    Days to FL  Day 1st Flower"!  call TFlower_init(CultivarID, TFi, SumRDi)
   
 ! Set sowing/start day of year for flowering model to start
   SDat = 41   !sowing date
-  ADAP = 0    !counter for number of days to anthesis
+  ADAP = 0    !counter for number of days from sowing to anthesis
+!  Initialize progress toward flowering, SumDRi, & Day of First Flower, Fdoy
+  SumRDi = 0.0
+  Fdoy = 0.0
   
-  open(20,File=Weather,Status='OLD')  !DSSAT format weather data
-  read(20,'(4/,a)') text              !skip first 5 lines
-  open(30,File="Anthesis.OUT",status='REPLACE')
-  write(30,'(a)') "adap yr doy  srad  tmin  tmax  SumRDi"
-
-  Dayi = 12.0  !temporarily set daylength to 12 hours
-
-! rate
+!Set constant values for weather inputs for sensitivity analysis
+  Dayi = 11.0  
+  Sradi = 12.0  
+  Tmaxi = 20.0
+  Tmini = 10.0
+  
+!Daylength (Dayi) variations loop
+  do m = 1,15
+  
+!Temperature variations simulation loop  
+  do k = 1,15
+! Development calls Rate Module in a time loop for integration (daily)
+  YR = 00
   do i = 1, 365
-    read(20,'(i5,3F6.0)') YRDOY, Sradi, Tmaxi, Tmini 
-    DOY = MOD(YRDOY,1000)
-    YR = (YRDOY - DOY)/1000
+  DOY = i
+      
     If (DOY > SDat-1) Then
       ADAP = ADAP + 1
+      YRDOY = SDat + ADAP  !no development on sowing day
       call TFlower_rate (Dayi, Sradi, Tmini, Tmaxi, TFi, SumRDi)
-      write(30,'(i4,1X,i2,1X,I3.3,3f6.2,f8.4)') ADAP, YR, DOY, Sradi, Tmini, Tmaxi, SumRDi
+      write(30,'(f4.1,1X,i2,1X,I3.3,3f6.2,f8.4)') ADAP, YR, DOY, Sradi, Tmini, Tmaxi, SumRDi
       if (SumRDi > 1.0) then
+        Fdoy = YRDOY
+        write(40,'(3x,A8,4X,4(f5.1,4X),f5.3,7x,2(f5.1,4x))') CultivarID,Dayi,Sradi,Tmini,Tmaxi,SumRDi,ADAP,Fdoy
+        
         exit
       endif
     endif
-  enddo
 
+  enddo
+      write(30,'(A8,4x,12(F5.2))') CultivarID, TFi 
+!   Reinitialize timer and integrator for delta temperature loop
+  Tmaxi  = Tmaxi + 1
+  Tmini  = Tmini + 1
+  SumRDi = 0.0
+  ADAP   = 0.0
+  enddo
+!   Increment Dayi; Reinitialize timer and integrator for delta daylength loop 
+  Tmaxi  = 20.0
+  Tmini  = 10.0
+  Dayi   = Dayi + 0.5
+  
+  SumRDi = 0.0
+  ADAP   = 0.0  
+  enddo
   Stop
 End Program Main
